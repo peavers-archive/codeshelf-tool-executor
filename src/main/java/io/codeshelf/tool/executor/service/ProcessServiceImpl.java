eccfee87190da73a57092aeed089e5b04aa9c631
Chris Turner <chris@forloop.space>
2019-09-05 21:30:19 +0100

    feature: start on docs

--- src/main/java/io/codeshelf/tool/executor/service/ProcessServiceImpl.java
+++ src/main/java/io/codeshelf/tool/executor/service/ProcessServiceImpl.java
@@ -24,6 +24,14 @@ public class ProcessServiceImpl implements ProcessService {
   @Value("${codeshelf.dry}")
   private boolean dry;
 
+  /**
+   * Handles the leg work of the tools. Executes the command passed in, then passes off the results
+   * to the {@link FirehoseService} to send the results to AWS Kinesis Firehose.
+   *
+   * @param command to execute through the process builder
+   * @param deliveryStream the firehose channel to send the output too
+   * @return the json representation of the output from the process builder
+   */
   @Override
   public String execute(final List<String> command, final String deliveryStream)
       throws IOException {
@@ -32,16 +40,11 @@ public class ProcessServiceImpl implements ProcessService {
 
     final ProcessBuilder processBuilder = new ProcessBuilder(command);
     final Process process = processBuilder.start();
-    final String output = consoleOutput(process.getInputStream());
-    final String error = consoleOutput(process.getErrorStream());
+    final String output = readConsole(process.getInputStream());
+    final String error = readConsole(process.getErrorStream());
     final String outputJson = convertToJson(output);
 
-    if (StringUtils.isNotBlank(error)) {
-      log.error(error);
-    } else {
-      log.info("raw {}", output);
-      log.info("json {}", outputJson);
-    }
+    logResult(output, error, outputJson);
 
     if (dry) {
       log.info("skipping push to firehose - codeshelf.dry=true");
@@ -55,12 +58,41 @@ public class ProcessServiceImpl implements ProcessService {
     return output;
   }
 
+  /**
+   * Simple helper method to output the results of the process builder to the console.
+   *
+   * @param output input stream from process builder
+   * @param error error stream from process builder
+   * @param outputJson the json representation of the output stream
+   */
+  private void logResult(final String output, final String error, final String outputJson) {
+
+    if (StringUtils.isNotBlank(error)) {
+      log.error(error);
+    } else {
+      log.info("raw {}", output);
+      log.info("json {}", outputJson);
+    }
+  }
+
+  /**
+   * Simple helper method to check if a string starts with '<', if so, convert it to Json
+   *
+   * @param xml string to convert to json
+   * @return json regardless of what is sent in
+   */
   private String convertToJson(final String xml) {
 
     return xml.startsWith("<") ? XML.toJSONObject(xml).toString() : xml;
   }
 
-  private String consoleOutput(final InputStream stream) {
+  /**
+   * Collect the result from the console and report it back.
+   *
+   * @param stream input stream from the process builder
+   * @return collection of all error messages
+   */
+  private String readConsole(final InputStream stream) {
 
     return new BufferedReader(new InputStreamReader(stream))
         .lines()
